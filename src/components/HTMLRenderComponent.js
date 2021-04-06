fb24e8b80e419676b9a71521c50a5ac990485014
Hanna <igerne94@gmail.com>
2021-04-06 12:31:24 +0200

    changes

--- src/components/HTMLRenderComponent.js
+++ src/components/HTMLRenderComponent.js
@@ -1,113 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-//import { CollapsibleComponent, CollapsibleHead, CollapsibleContent } from "react-collapsible-component";
-import { enviroments } from './config.ts';
-import HTMLRender from './components/HTMLRenderComponent.js';
+import { CollapsibleComponent, CollapsibleHead, CollapsibleContent } from "react-collapsible-component";
 
-class MyPage extends React.Component {
 
-  constructor(props) {
-    super(props);
+class HTMLRender extends React.Component {
 
-    this.state = {
-      uglyId: '',
-      codeSystem: '',
-      code: '',
-      url: '',
-      response: '',
-      records: [],
-      enviroment: 'prod',
-
-    };
-  
-  }
-
-  
-
-  mySubmitHandler = (event) => {
-    event.preventDefault();
-
-    //const urlAddress = 'https://api.helsedirektoratet.no/innhold/innhold';
-    const enviroment = this.state.enviroment;
-    let setEnviroments = enviroments.find(o => o.id === enviroment);
-
-    let url = setEnviroments.url;
-    let key = setEnviroments.key
-    if (this.state.uglyId) {
-      url += '/' + this.state.uglyId;
-    } else {
-      url += '?kodeverk=' + this.state.codeSystem + "&kode=" + this.state.code;
-    } 
-    
-   
-
-    this.setState({ url: url });
-
-    fetch(url,
-      {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Ocp-Apim-Subscription-Key': key
-        }
-      }
-    )
-      .then(response => response.json())
-      .then(data => this.responseHandler(data));
-  }
-
-  responseHandler = (data) => {
-    if (data) {
-      this.setState({
-        response: JSON.stringify(data, null, 2)
-      });
-    }
-  }
-
-  myChangeHandler = (event) => {
-    this.setState({
-      uglyId: event.target.value
-    });
-  }
-  ChangeHandlerCode = (event) => {
-    this.setState({
-      code: event.target.value
-    });
-  }
-  ChangeHandlerCodeSystem = (event) => {
-    this.setState({
-      codeSystem: event.target.value
-    });
-  }
-  ChangeHandlerEnviroment = (event) => {
-    this.setState({
-      enviroment: event.target.value
-    
-    });
-  }
-
- /* renderJson() {
+  renderJson() {
     if (this.state.response) {
       let json = JSON.parse(this.state.response);
       console.log(json);
 
       if (Array.isArray(json)) {
         console.log(json);
+
         return json.map((item, index) =>
           <div key={index}>
-
 
             <div><h1>{item.tittel}</h1></div>
             <div dangerouslySetInnerHTML={{ __html: item.tekst }}></div>
             <CollapsibleComponent name={item.id}> 
-            {item.data.rasjonale != null ? <CollapsibleHead><h2>Rasjonale</h2></CollapsibleHead>:null} 
-              <CollapsibleContent><div dangerouslySetInnerHTML={{ __html: item.data.rasjonale }}></div>
+              {item.data.rasjonale != null ? <CollapsibleHead><h2>Rasjonale</h2></CollapsibleHead>:''} 
+
+              <CollapsibleContent>
+                <div dangerouslySetInnerHTML={{ __html: item.data.rasjonale }}></div>
               </CollapsibleContent>
-      
+
               <CollapsibleHead><h2>Metadata</h2></CollapsibleHead>
               <CollapsibleContent>
+
                 <table><tbody>
 
                   <tr>
@@ -131,8 +50,9 @@ class MyPage extends React.Component {
                   </tr>
 
                 </tbody></table>
-                </CollapsibleContent>
-                </CollapsibleComponent>
+
+              </CollapsibleContent>
+            </CollapsibleComponent>
 
           </div>);
       } else {
@@ -149,8 +69,9 @@ class MyPage extends React.Component {
             <div dangerouslySetInnerHTML={{ __html: item.tekst }}></div>
             <CollapsibleComponent>
             {item.data.rasjonale != null ? <CollapsibleHead><h2>Rasjonale</h2></CollapsibleHead>:null} 
-             <CollapsibleContent><div dangerouslySetInnerHTML={{ __html: item.data.rasjonale }}></div></CollapsibleContent>
+            <CollapsibleContent><div dangerouslySetInnerHTML={{ __html: item.data.rasjonale }}></div></CollapsibleContent>
               <CollapsibleHead><h2>Metadata</h2></CollapsibleHead>
+
               <CollapsibleContent>
                 <table><tbody>
 
@@ -229,11 +150,12 @@ class MyPage extends React.Component {
           </div>);
       }
     }
-    return '';
-  }*/
+  return '';
+  }
 
 
- /* renderLinks(links) {
+  renderLinks(links) {
+    if (links != null)
     return links.map((item, index) =>
       <div key={index}>
 
@@ -255,75 +177,16 @@ class MyPage extends React.Component {
             <td>Struktur Id</td><td>{item.strukturId ? item.strukturId : ''}</td>
           </tr>
 
-
         </tbody></table>
 
       </div>);
   }
-*/
+
 
   render() {
-    return (
-      <div>
-
-        <form onSubmit={this.mySubmitHandler}>
-        <select name="enviroment" id="enviroment"
-            onChange={evt => this.ChangeHandlerEnviroment(evt)}
-          >
-         <option value="prod">Production</option>
-            <option value="test">Test</option>
-            <option value="qa">QA</option>
-            
-          </select>
-          <p>Please provide either HAPI-id or code from a code system</p>
-          <input
-            id="id"
-            type='text'
-            autoComplete="on"
-            placeholder="HAPI-id"
-            value={this.state.uglyId}
-            onChange={evt => this.myChangeHandler(evt)}
-          />
-
-          <span className="marginRight">or</span>
-
-          <select name="codeSystem" id="codeSystem"
-            onChange={evt => this.ChangeHandlerCodeSystem(evt)}
-          >
-            <option value="" select="default">Choose code system</option>
-            <option value="ICD-10">ICD-10</option>
-            <option value="ICPC-2">ICPC-2</option>
-            <option value="ATC">ATC</option>
-            <option value="SNOMED-CT">SNOMED-CT</option>
-          </select>
-
-          <input
-            type='text'
-            autoComplete="on"
-            id="code"
-            placeholder="Code"
-            value={this.state.code}
-            onChange={evt => this.ChangeHandlerCode(evt)}
-          />
-
-          <input
-            type='submit'
-            value="поиск"
-          />
-        </form>
-
-
-       <div><HTMLRender/></div>
-        <div>here is the JSON part</div>
-        <div><pre>{this.state.response}</pre></div>
-        <div><pre><h4>{this.state.url}</h4></pre></div>
-
-      </div>
-    );
+    return <div><h1>{HTMLRender}</h1></div>
   }
 
 }
 
-ReactDOM.render(<MyPage />, document.getElementById('root'));
-
-reportWebVitals();
+export default HTMLRender;
