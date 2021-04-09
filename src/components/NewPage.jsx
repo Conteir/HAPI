9371bf913998d7275a86b7f2173947f239068095
Hanna <igerne94@gmail.com>
2021-04-09 07:54:04 +0200

    small changes

--- src/components/NewPage.jsx
+++ src/components/NewPage.jsx
@@ -14,12 +14,9 @@ export const NewPage = class NewPage extends React.Component {
                   />
                 </div>
                 <div className="col-lg-5">
-                  <h1 className="font-weight-light">New page</h1>
+                  <h1 className="font-weight-light">This is a new page</h1>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of
-                    type and scrambled it to make a type specimen book.
+                    Hello, Eirik:)
                   </p>
                 </div>
               </div>
