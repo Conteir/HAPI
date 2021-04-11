6a389c570ccb975b7a76579ad83e2a596af57a48
Eirik Hafver Rønjum <64041578+eirikconteir@users.noreply.github.com>
2021-04-11 21:46:58 +0200

    Added new page for documentation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NewPage } from './components/NewPage.jsx';
 import { HomePage } from './components/HomePage.jsx';
+import { Documentation } from './components/Documentation.jsx';
 
 export const App = class App extends React.Component {
   
@@ -16,6 +17,7 @@ export const App = class App extends React.Component {
               <div className="nav nav-tabs" id="nav-tab" role="tablist">
                 <a className="nav-item nav-link active" id="nav-homepage-tab" data-toggle="tab" href="/" role="tab" aria-controls="nav-homepage" aria-selected="true">Search HAPI</a>
                 <a className="nav-item nav-link" id="nav-newpage-tab" data-toggle="tab" href="/newpage" role="tab" aria-controls="nav-newpage" aria-selected="false">Get ID</a>
+                <a className="nav-item nav-link" id="nav-documentation-tab" data-toggle="tab" href="/documentation" role="tab" aria-controls="nav-documentation" aria-selected="false">Documentation</a>
               </div>
             </nav>
 
@@ -25,6 +27,9 @@ export const App = class App extends React.Component {
               </Route>
               <Route path="/newpage" component={NewPage}>
                 <NewPage />
+              </Route>
+              <Route path="/documentation" component={Documentation}>
+                <Documentation />
               </Route>
             </Switch>
 
